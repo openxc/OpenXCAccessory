d06a312a3e881cbbb7601bec6254508dc682c4d2
Matt@Work <carpenma@gmail.com>
2016-05-27 17:19:59 +0200

    Adding symbolic link script to modem branch as well
    Same info as file on master branch.  Converts /etc files to symbolic links

---
+++ etc/create_symlinks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script removes the existing files in /etc which are to be housed within the OpenXCAccessory repository.  The files are then replaced by symbolic links to the new versions within /root/OpenXCAccessory/etc
+
+rm /etc/wpa_supplicant*	# Remove the existing supplicant files in /etc to make way for symlinks
+ln -s /root/OpenXCAccessory/etc/wpa_supplicant.conf /etc/wpa_supplicant.conf
+ln -s /root/OpenXCAccessory/etc/wpa_supplicant.conf /etc/wpa_supplicant_modem.conf
+ln -s /root/OpenXCAccessory/etc/wpa_supplicant.conf /etc/wpa_supplicant_rsu.conf
+ln -s /root/OpenXCAccessory/etc/wpa_supplicant.conf /etc/wpa_supplicant_v2x.conf
+ln -s /root/OpenXCAccessory/etc/wpa_supplicant.conf /etc/wpa_supplicant_v2x_top2.conf
+rm -r /etc/init.d
+ln -s /root/OpenXCAccessory/etc/init.d/ /etc/init.d
+rm /etc/rc.local
+ln -s /root/OpenXCAccessory/etc/rc.local /etc/rc.local
+rm /etc/pppd_script
+ln -s /root/OpenXCAccessory/startup/pppd_script /etc/pppd_script
